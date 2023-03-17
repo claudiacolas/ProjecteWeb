736b0848355f01da1279957814d9d8062cfaddd1
claudiacolas <claudiacolastorres@gmail.com>
2023-03-17 17:12:20 +0000

    "Added models"

--- mycombinations/web/models.py
+++ mycombinations/web/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Marca(models.Model):
+    name = models.CharField(max_length=200)
+
+class Alcohol(models.Model):
+    name = models.CharField(max_length=200)
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    
+class Barreja(models.Model):
+    name = models.CharField(max_length=200)
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+
+class Combinacio(models.Model):
+    name = models.CharField(max_length=200)
+    alcohol = models.ForeignKey(Alcohol, on_delete=models.CASCADE)
+    barreja = models.ForeignKey(Barreja, on_delete=models.CASCADE)
