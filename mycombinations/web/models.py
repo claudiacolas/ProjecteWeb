6a293699ed4fcdc334d700339431068a174dbcb6
claudiacolas <claudiacolastorres@gmail.com>
2023-03-20 20:14:31 +0000

    Adding new brands

--- mycombinations/web/models.py
+++ mycombinations/web/models.py
@@ -4,16 +4,28 @@ from django.db import models
 
 class Brand(models.Model):
     name = models.CharField(max_length=200)
+    
+    def __str__(self) -> str:
+        return self.name
 
 class Alcohol(models.Model):
     name = models.CharField(max_length=200)
-    marca = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    
+    def __str__(self) -> str:
+        return self.name+" of the brand "+self.brand.name
     
 class Mix(models.Model):
     name = models.CharField(max_length=200)
-    marca = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    
+    def __str__(self) -> str:
+        return self.name
 
 class Combination(models.Model):
     name = models.CharField(max_length=200)
     alcohol = models.ForeignKey(Alcohol, on_delete=models.CASCADE)
-    barreja = models.ForeignKey(Mix, on_delete=models.CASCADE)
+    mix = models.ForeignKey(Mix, on_delete=models.CASCADE)
+    
+    def __str__(self) -> str:
+        return self.name
