f433e07615f2325a0d1179b648b8d30a21aa99e6
claudiacolas <claudiacolastorres@gmail.com>
2023-03-20 11:27:22 +0000

    Database init

--- mycombinations/web/admin.py
+++ mycombinations/web/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from web.models import Marca, Alcohol, Barreja, Combinacio
+
 # Register your models here.
+
+admin.site.register(Marca)
+admin.site.register(Alcohol)
+admin.site.register(Barreja)
+admin.site.register(Combinacio)
