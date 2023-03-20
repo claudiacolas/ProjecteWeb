2d16554ece6a0359bc8db015e83cefb5ded48505
claudiacolas <claudiacolastorres@gmail.com>
2023-03-20 19:49:11 +0000

    Model names

--- mycombinations/web/models.py
+++ mycombinations/web/models.py
@@ -2,18 +2,18 @@ from django.db import models
 
 # Create your models here.
 
-class Marca(models.Model):
+class Brand(models.Model):
     name = models.CharField(max_length=200)
 
 class Alcohol(models.Model):
     name = models.CharField(max_length=200)
-    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    marca = models.ForeignKey(Brand, on_delete=models.CASCADE)
     
-class Barreja(models.Model):
+class Mix(models.Model):
     name = models.CharField(max_length=200)
-    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    marca = models.ForeignKey(Brand, on_delete=models.CASCADE)
 
-class Combinacio(models.Model):
+class Combination(models.Model):
     name = models.CharField(max_length=200)
     alcohol = models.ForeignKey(Alcohol, on_delete=models.CASCADE)
-    barreja = models.ForeignKey(Barreja, on_delete=models.CASCADE)
+    barreja = models.ForeignKey(Mix, on_delete=models.CASCADE)
