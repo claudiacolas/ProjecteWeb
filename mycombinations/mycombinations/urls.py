5cd5c0d95a91c9be2a3ebc6e3526b489f0e108d1
claudiacolas <claudiacolastorres@gmail.com>
2023-03-20 21:10:42 +0000

    Creating views

--- mycombinations/mycombinations/urls.py
+++ mycombinations/mycombinations/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import web.views
+
 urlpatterns = [
+    path('', web.views.principal),
     path("admin/", admin.site.urls),
 ]
--- mycombinations/web/views.py
+++ mycombinations/web/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from web.models import Brand, Alcohol, Mix, Combination
 
 # Create your views here.
+
+def principal(request):
+    brands = Brand.objects.all()
+    resposta = [brand.name for brand in brands]
+    html = "<br />".join(resposta)
+    return HttpResponse(html)
+    
