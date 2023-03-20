6d04378f609e505ad43e0027f3fd585c0864f65c
claudiacolas <claudiacolastorres@gmail.com>
2023-03-20 19:53:27 +0000

    Admin changes

--- mycombinations/web/admin.py
+++ mycombinations/web/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from web.models import Marca, Alcohol, Barreja, Combinacio
+from web.models import Brand, Alcohol, Mix, Combination
 
 # Register your models here.
 
-admin.site.register(Marca)
+admin.site.register(Brand)
 admin.site.register(Alcohol)
-admin.site.register(Barreja)
-admin.site.register(Combinacio)
+admin.site.register(Mix)
+admin.site.register(Combination)
