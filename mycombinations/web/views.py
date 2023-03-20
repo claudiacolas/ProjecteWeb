8f7f12f414e998deb0daeff4b842c2847b1df739
claudiacolas <claudiacolastorres@gmail.com>
2023-03-20 21:51:09 +0000

    Updates

--- mycombinations/web/views.py
+++ mycombinations/web/views.py
@@ -9,3 +9,6 @@ def principal(request):
     brands = Brand.objects.all()
     return render(request, 'web/index.html', {"brands": brands})
     
+def brand(request, brand_id):
+    brand = Brand.objects.get(pk=brand_id)
+    return render(request, 'web/brand.html', {"brand": brand})
