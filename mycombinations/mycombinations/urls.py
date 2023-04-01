f439162407351aa2e7332cf63aa6ceb9c536241c
BielGuallar <bielguallar@gmail.com>
2023-04-01 20:16:13 +0000

    Canvis a views.py, urls.py, .html

--- mycombinations/mycombinations/urls.py
+++ mycombinations/mycombinations/urls.py
@@ -19,6 +19,9 @@ from django.urls import path
 import web.views
 
 urlpatterns = [
-    path('', web.views.principal),
+    path('', web.views.principal, name='Principal'),
+    path('combination/<int:bar_id>', web.views.combination, name='Combination'),
+    path('alcohol/<int:pk>', web.views.AlcoholView.as_view(), name="Alcohol"),
+    path('mix/<int:pk>', web.views.MixView.as_view(), name="Mix"),
     path("admin/", admin.site.urls),
 ]
--- mycombinations/web/views.py
+++ mycombinations/web/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
 
 from web.models import Brand, Alcohol, Mix, Combination
 
 # Create your views here.
 
 def principal(request):
-    brands = Brand.objects.all()
-    return render(request, 'web/index.html', {"brands": brands})
+    Combination = Combination.objects.all()
+    return render(request, 'web/index.html', {"combination": combination})
     
-def brand(request, brand_id):
-    brand = Brand.objects.get(pk=brand_id)
-    return render(request, 'web/brand.html', {"brand": brand})
+def combination(request, combination_id):
+    Combination = Combination.objects.get(pk=combination_id)
+    return render(request, 'web/combination.html', {"combination": combination})
+class AlcoholView(generic.DetailView):
+    model = Alcohol
+    template_name = 'web/alcohol.html'
+class MixView(generic.DetailView):
+    model = Mix
+    template_name = 'web/mix.html'
