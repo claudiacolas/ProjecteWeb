ca76728217bf9f3b0f8e32a41a6451bc4d9e5b91
francescginep <francescginep@gmail.com>
2023-04-02 21:08:45 +0000

    urls.py i diferents templates

--- mycombinations/web/views.py
+++ mycombinations/web/views.py
@@ -14,9 +14,25 @@ def principal(request):
 def combination(request, combination_id):
     combination = Combination.objects.get(pk=combination_id)
     return render(request, 'web/combination.html', {"combination": combination})
+
+def Alcohols(request):
+    alcohols = Alcohol.objects.all()
+    return render(request, 'web/alcohol.html', {"alcohols": alcohols})
+
+def Mixs(request):
+    mixs= Mix.objects.all()
+    return render(request, 'web/mix.html', {'mixs': mixs})
+
+def Brands(request):
+    brands= Brand.objects.all()
+    return render(request, 'web/brand.html', {'brands': brands})
+
 class AlcoholView(generic.DetailView):
     model = Alcohol
     template_name = 'web/alcohol.html'
 class MixView(generic.DetailView):
     model = Mix
     template_name = 'web/mix.html'
+class BrandView(generic.DetailView):
+    model = Brand
+    template_name = 'web/brand.html'
