aad652c055c059d431c9db9d2421140472539b0e
claudiacolas <claudiacolastorres@gmail.com>
2023-04-25 10:50:07 +0200

    Updates in models

--- mycombinations/web/models.py
+++ mycombinations/web/models.py
@@ -35,4 +35,4 @@ class Combination(models.Model):
     mix = models.ForeignKey(Mix, on_delete=models.CASCADE)
     
     def __str__(self) -> str:
-        return self.name+" = " +self.alcohol.name+" with "+self.mix.name
+        return self.name+" with " +self.alcohol.name+" and "+self.mix.name
