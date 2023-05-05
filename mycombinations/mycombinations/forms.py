98834dda31a277206362480c38e478ef43a4e270
Àngela Giribet <angelagiribet@gmail.com>
2023-05-05 16:38:04 +0200

    changes in views.py, urls.py, models.py and templates/web

---
+++ mycombinations/mycombinations/forms.py
@@ -0,0 +1,22 @@
+from django.forms import ModelForm
+from web.models import Brand, Alcohol, Mix, Combination
+
+class BrandForm(ModelForm):
+    class Meta:
+        model = Brand
+        fields = ['name', 'type']
+
+class AlcoholForm(ModelForm):
+    class Meta:
+        model = Alcohol
+        fields = ['name', 'brand']
+
+class MixForm(ModelForm):
+    class Meta:
+        model = Mix
+        fields = ['name', 'brand']
+
+class CombinationForm(ModelForm):
+    class Meta:
+        model = Combination
+        fields = ['name', 'alcohol', 'mix']
