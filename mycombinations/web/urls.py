14a561386dc1045ccefa93766e266681f0a89436
francescginep <francescginep@gmail.com>
2023-05-10 18:52:59 +0200

    PODER EDITAR I VEURE LES COMBINACIONS CREADES PER JO (USUARI)

--- mycombinations/web/urls.py
+++ mycombinations/web/urls.py
@@ -16,8 +16,18 @@ urlpatterns = [
          ListView.as_view(
              queryset=Combination.objects.filter(date__lte=timezone.now()).order_by('-date')[:5],
              context_object_name='latest_combination_list',
-             template_name='combination_list.html'),
+             template_name='web/combination_list.html'),
          name='combination_list'),
+    path('',ListView.as_view(
+             queryset=Alcohol.objects.filter(date__lte=timezone.now()).order_by('-date')[:5],
+             context_object_name='latest_alcohol_list',
+             template_name='web/alcohol_list.html'),
+         name='alcohol_list'),
+    path('',ListView.as_view(
+             queryset=Mix.objects.filter(date__lte=timezone.now()).order_by('-date')[:5],
+             context_object_name='latest_mix_list',
+             template_name='web/mix_list.html'),
+         name='mix_list'),
 
     # Combination details, ex.: /mycombinations/combinations/1/
     path('combinations/<int:pk>',
