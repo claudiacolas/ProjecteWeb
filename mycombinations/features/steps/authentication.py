79faf28768042a99af266870c67f0aba7e2125e7
Roberto García <roberto@rhizomik.net>
2023-05-15 16:47:46 +0200

    Added missing steps, now Register Brand feature passes

--- mycombinations/features/steps/authentication.py
+++ mycombinations/features/steps/authentication.py
@@ -9,7 +9,6 @@ def step_impl(context, username, password):
     from django.contrib.auth.models import User
     User.objects.create_user(username=username, email='user@example.com', password=password)
 
-
 @given('I login as user "{username}" with password "{password}"')
 def step_impl(context, username, password):
     context.browser.visit(context.get_url('/accounts/login/?next=/mycombinations/'))
@@ -17,3 +16,12 @@ def step_impl(context, username, password):
     context.browser.fill('username', username)
     context.browser.fill('password', password)
     form.find_by_value('login').first.click()
+
+@given('I\'m not logged in')
+def step_impl(context):
+    context.browser.visit(context.get_url('logout')+'?next=/mycombinations/')
+    assert context.browser.is_text_present('login')
+
+@then("I'm redirected to the login form")
+def step_impl(context):
+    assert context.browser.url.startswith(context.get_url('login'))
