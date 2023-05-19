de17c2e1f28062be7bf68c479f9234fe5b7b9d30
rikii02 <rikigodia@gmail.com>
2023-05-19 15:43:16 +0000

    Api_tryout

---
+++ mycombinations/web/static/js/autocomplete.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    $.getJSON("/static/drinks.json", {}, function (drinks) {
+        $("#id_name").autocomplete({
+            source: drinks
+        });
+    });
+
+    $("#id_name").autocomplete({
+        source: function( request, response ) {
+            $.ajax({
+                url: "www.thecocktaildb.com/api/json/v1/1/search.php?i",
+                data: {
+                    name_startsWith: request.term,
+                },
+                success: function( data ) {
+                    response( $.map( data.ingredient, function( item ) {
+                        return { 
+                            label: item.strIngredient1,
+                            value: item.strIngredient1
+                        }
+                    }));
+                }
+            });
+        },
+        minLength: 2,
+        select: function( event, ui ) {
+            if (ui.item) {
+                $("#id_value").val(ui.item.strIngredient1)
+            }
+        }
+    });
+});
